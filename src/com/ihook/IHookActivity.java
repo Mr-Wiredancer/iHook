823d45da1dc96db1e017de083cbce1347ed05e7a
Jiahao Li <lijiahao90@gmail.com>
2011-10-25 19:16:18 -0700

    test3

--- src/com/ihook/IHookActivity.java
+++ src/com/ihook/IHookActivity.java
@@ -2,7 +2,7 @@ package com.ihook;
 
 import android.app.Activity;
 import android.os.Bundle;
-//test2
+//test3
 public class IHookActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
